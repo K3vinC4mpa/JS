9b099c3c83a40bc8f104eca31b250d0e37d9c9ff
Kevin Campa <k3vinc4mpa@gmail.com>
2022-11-24 14:51:18 -0700

    Rework Rock-Paper-Scissors project

--- Rock-Paper-Scissors/app.js
+++ Rock-Paper-Scissors/app.js
@@ -1,57 +1,64 @@
-const computerChoiceDisplay = document.getElementById('computer-choice');
-const userChoiceDisplay = document.getElementById('your-choice');
-const resultDisplay = document.getElementById('result');
-const possibleChoices = document.querySelectorAll('button');
+const choices = ["rock", "paper", "scissors"];
 
-let userChoice
-let computerChoice
-let result
+const computerChoiceDisplay = document.getElementById("computer-choice");
+const userChoiceDisplay = document.getElementById("your-choice");
+const resultDisplay = document.getElementById("result");
+const possibleChoices = document.querySelectorAll("button");
 
-possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener('click', (e)=>{
-    userChoice = e.target.id;
-    userChoiceDisplay.innerHTML = userChoice;
-    generateComputerChoice();
-    getResult()
+possibleChoices.forEach((yourChoice) =>
+  yourChoice.addEventListener("click", (e) => {
+    clearValues();
 
-}))
+    // asign the value of our choice to a variable and display it
+    const userChoice = e.currentTarget.id;
+    userChoiceDisplay.textContent = userChoice;
 
-function generateComputerChoice (){
-    const randomNumber = Math.floor(Math.random() * possibleChoices.length) + 1; // or 3
-    console.log(randomNumber)
-    
-    if (randomNumber === 1){
-         computerChoice = "rock";
-    }
-    if(randomNumber === 2){
-        computerChoice = "paper";
-    }
-    if (randomNumber === 3) {
-        computerChoice = "scissors";
-    }
-    computerChoiceDisplay.innerHTML = computerChoice;
+    setTimeout(() => {
+      // asign the value of computer choice randomly into a variable and display it.
+      const computerChoice = choices[generateComputerChoice()];
+      computerChoiceDisplay.innerHTML = computerChoice;
+
+      // call the function that determines the result of match.
+      getResult(userChoice, computerChoice);
+    }, 1500);
+  })
+);
+
+// generate a  random number for the choices array
+function generateComputerChoice() {
+  return Math.floor(Math.random() * choices.length);
 }
 
-function getResult(){
-    if (computerChoice === userChoice){
-        result = "It's a draw boi....."
-    }
-    else if(computerChoice === 'rock' && userChoice === 'paper'){
-        result = "You wins!!!"
-    }
-    else if (computerChoice === 'rock' && userChoice === 'scissors') {
-        result = "You lose."
-    }
-    else if (computerChoice === 'paper' && userChoice === 'rock') {
-        result = "You lose."
-    }
-    else if (computerChoice === 'paper' && userChoice === 'scissors') {
-        result = "You wins!!!"
-    }
-    else if (computerChoice === 'scissors' && userChoice === 'paper') {
-        result = "You lose."
-    }
-    else if (computerChoice === 'scissors' && userChoice === 'rock') {
-        result = "You wins!!!"
-    }
-    resultDisplay.innerHTML = result;
+// Determines the result of match
+function getResult(userChoice, computerChoice) {
+  if (computerChoice === userChoice) {
+    result = "It's a draw";
+  } else if (computerChoice === "rock" && userChoice === "paper") {
+    result = "You wins!!!";
+  } else if (computerChoice === "rock" && userChoice === "scissors") {
+    result = "You lose.";
+  } else if (computerChoice === "paper" && userChoice === "rock") {
+    result = "You lose.";
+  } else if (computerChoice === "paper" && userChoice === "scissors") {
+    result = "You wins!!!";
+  } else if (computerChoice === "scissors" && userChoice === "paper") {
+    result = "You lose.";
+  } else if (computerChoice === "scissors" && userChoice === "rock") {
+    result = "You wins!!!";
+  }
+
+  if (result === `You wins!!!`) {
+    document.body.style.backgroundColor = "green";
+  } else if (result === `You lose.`) {
+    document.body.style.backgroundColor = "red";
+  } else {
+    document.body.style.backgroundColor = "grey";
+  }
+  resultDisplay.innerHTML = result;
+}
+
+function clearValues() {
+  computerChoiceDisplay.textContent = "";
+  userChoiceDisplay.textContent = "";
+  resultDisplay.textContent = "";
 }
